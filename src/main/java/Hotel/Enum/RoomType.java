6cfdfea176105acd3d1d0a8b6a0b23be15c6fa4e
Dave Ellis <dave@ellisnetmail.co.uk>
2018-01-12 15:18:24 +0000

    Added RoomType class with test for getOccupancy

--- src/main/java/Hotel/Enum/RoomType.java
+++ src/main/java/Hotel/Enum/RoomType.java
@@ -1,4 +1,19 @@
 package Hotel.Enum;
 
-public class RoomType {
+public enum RoomType {
+
+    SINGLE(1),
+    DOUBLE(2),
+    FAMILY(4);
+
+    private final int occupancy;
+
+    RoomType(int occupancy){
+        this.occupancy = occupancy;
+    }
+
+    public int getOccupancy(){
+        return occupancy;
+    }
+
 }
--- src/test/java/RoomTypeTest.java
+++ src/test/java/RoomTypeTest.java
@@ -1,6 +1,15 @@
 import Hotel.Enum.RoomType;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class RoomTypeTest {
 
-    RoomType roomtype;
+    RoomType roomType;
+
+    @Test
+    public void canGetRoomOccupancy(){
+        assertEquals(1, roomType.SINGLE.getOccupancy());
+    }
 }
