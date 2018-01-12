46ecd626b3f97b163c2e59ca7c5e2454a7a39cfd
Dave Ellis <dave@ellisnetmail.co.uk>
2018-01-12 15:59:42 +0000

    Added getRate method to ConferenceRoom class with test

--- src/main/java/Hotel/Room/ConferenceRoom.java
+++ src/main/java/Hotel/Room/ConferenceRoom.java
@@ -3,13 +3,19 @@ package Hotel.Room;
 public class ConferenceRoom extends Room{
 
     private String roomName;
+    private double rate;
 
-    public ConferenceRoom(int capacity, String roomName){
+    public ConferenceRoom(int capacity, String roomName, double rate){
         super(capacity);
         this.roomName = roomName;
+        this.rate = rate;
 
     }
     public String getRoomName() {
         return roomName;
+    }
+
+    public double getRate() {
+        return rate;
     }
 }
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -10,11 +10,16 @@ public class ConferenceRoomTest {
 
     @Before
     public void before(){
-        conferenceRoom = new ConferenceRoom(20, "Lomond");
+        conferenceRoom = new ConferenceRoom(20, "Lomond", 2000.00);
     }
 
     @Test
     public void hasRoomName(){
         assertEquals("Lomond", conferenceRoom.getRoomName());
+    }
+
+    @Test
+    public void hasRate(){
+        assertEquals(2000.00, conferenceRoom.getRate(), 0.01);
     }
 }
