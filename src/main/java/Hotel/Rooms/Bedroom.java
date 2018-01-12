cbdb95ff3709cddaace8eab0eca92ac5f4dd6e64
Dave Ellis <dave@ellisnetmail.co.uk>
2018-01-12 14:29:48 +0000

    Added Bedroom class with test for roomNumber property

---
+++ src/main/java/Hotel/Rooms/Bedroom.java
@@ -0,0 +1,14 @@
+package Hotel.Rooms;
+
+public class Bedroom {
+
+    private int roomNumber;
+
+    public Bedroom(int roomNumber){
+        this.roomNumber = roomNumber;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+}
---
+++ src/test/java/BedroomTest.java
@@ -0,0 +1,20 @@
+import Hotel.Rooms.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BedroomTest {
+
+    Bedroom bedroom;
+
+    @Before
+    public void before(){
+        bedroom = new Bedroom(1);
+    }
+
+    @Test
+    public void hasRoomNumber(){
+        assertEquals(1, bedroom.getRoomNumber());
+    }
+}
