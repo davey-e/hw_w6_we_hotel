2da60b7d76ae2a392098183b3543e61d047398c2
Dave Ellis <dave@ellisnetmail.co.uk>
2018-01-13 21:16:25 +0000

    Added Hotel class with test for name property

---
+++ src/main/java/Hotel/Hotel.java
@@ -0,0 +1,14 @@
+package Hotel;
+
+public class Hotel {
+
+    String name;
+
+    public Hotel(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/HotelTest.java
@@ -0,0 +1,21 @@
+import Hotel.Hotel;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HotelTest {
+
+    Hotel hotel;
+
+    @Before
+    public void before(){
+        hotel = new Hotel("Faulty Towers");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Faulty Towers", hotel.getName());
+    }
+
+}
