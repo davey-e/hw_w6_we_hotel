3252559655f1ea5446de22d040a5aff1932c6b2c
Dave Ellis <dave@ellisnetmail.co.uk>
2018-01-13 21:30:03 +0000

    Added bedrooms ArrayList property to Hotel class along with getBedrooms method and test

--- src/main/java/Hotel/Hotel.java
+++ src/main/java/Hotel/Hotel.java
@@ -1,14 +1,24 @@
 package Hotel;
 
+import Hotel.Room.Bedroom;
+
+import java.util.ArrayList;
+
 public class Hotel {
 
-    String name;
+    private String name;
+    private ArrayList<Bedroom> bedrooms;
 
-    public Hotel(String name){
+    public Hotel(String name, ArrayList<Bedroom> bedrooms){
         this.name = name;
+        this.bedrooms = bedrooms;
     }
 
     public String getName() {
         return name;
+    }
+
+    public ArrayList<Bedroom> getBedrooms() {
+        return bedrooms;
     }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,21 +1,38 @@
+import Hotel.Enum.RoomType;
 import Hotel.Hotel;
+import Hotel.Room.Bedroom;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
 public class HotelTest {
 
     Hotel hotel;
+    Bedroom bedroom1;
+    Bedroom bedroom2;
+    ArrayList<Bedroom> bedrooms;
 
     @Before
     public void before(){
-        hotel = new Hotel("Faulty Towers");
+        bedroom1 = new Bedroom(1, RoomType.SINGLE, 50.00);
+        bedroom2 = new Bedroom(2, RoomType.DOUBLE, 80.00);
+        bedrooms = new ArrayList<>();
+        bedrooms.add(bedroom1);
+        bedrooms.add(bedroom2);
+        hotel = new Hotel("Faulty Towers", bedrooms);
     }
 
     @Test
     public void hasName(){
         assertEquals("Faulty Towers", hotel.getName());
+    }
+
+    @Test
+    public void hasBedroomsArrayList(){
+        assertEquals(bedrooms, hotel.getBedrooms());
     }
 
 }
